b65b8d37203d63596a4a9d160bc3b61b2505b6db
Krzysztof Antczak <antczakmd@gmail.com>
2022-08-15 07:29:21 +0100

    Javaintro

--- kodila-intro/src/main/java/com/kodilla/kodilaintro/Loops.java
+++ kodila-intro/src/main/java/com/kodilla/kodilaintro/Loops.java
@@ -1,16 +1,23 @@
 package com.kodilla.kodilaintro;
-
+//
 public class Loops {
-    public static void main(String[] args){
-            String[] car = new String[5];
-            car[0] = "Jetta";
-            car[1] = "Mx5";
-            car[2] = "T3";
-            car[3] = "Ix20";
-            car[4] = "Panda";
-        for (String item : car){
-        System.out.println(item);
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
         }
     }
 }
+
+
+        //            String[] car = new String[5];
+//            car[0] = "Jetta";
+//            car[1] = "Mx5";
+//            car[2] = "T3";
+//            car[3] = "Ix20";
+//            car[4] = "Panda";
+//        for (String item : car){
+//        System.out.println(item);
+//        }
+//    }
+//}
 
--- kodila-intro/src/main/java/com/kodilla/kodilaintro/Main.java
+++ kodila-intro/src/main/java/com/kodilla/kodilaintro/Main.java
@@ -3,20 +3,33 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-        Grades grades = new Grades();
+        User user = new User();
 
         Scanner input = new Scanner(System.in);
 
-        for (int i=0; i < 10; i++) {
-            System.out.println("Wprowadz liczbe "+(i+1));
+        for (int i=0; i < user.averageOfAges; i++) {
+            System.out.println("Wprowadz wiek "+(i+1));
             int grade = input.nextInt();
-            grades.add(grade);
+            user.add(value);
         }
-        System.out.println("Ostatnia ocena" + grades.theLastOne());
-        System.out.println("Srednia ocen" + grades.averageOfGrades());
+        System.out.println("Srednia ocen" + user.averageOfAges());
 
-    }
-}
+//public class Main{
+//    public static void main(String[] args) {
+//        Grades grades = new Grades();
+//
+//        Scanner input = new Scanner(System.in);
+//
+//        for (int i=0; i < 10; i++) {
+//            System.out.println("Wprowadz liczbe "+(i+1));
+//            int grade = input.nextInt();
+//            grades.add(grade);
+//        }
+//        System.out.println("Ostatnia ocena" + grades.theLastOne());
+//        System.out.println("Srednia ocen" + grades.averageOfGrades());
+//
+//    }
+//}
 //    public static void main(String[] args) {
 //        String myName = "Maciej";
 //        System.out.println(myName);
