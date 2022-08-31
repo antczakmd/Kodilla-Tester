d2ef4ab286004d8b4a417346fd06ab4bb30d5b66
Krzysztof Antczak <antczakmd@gmail.com>
2022-08-31 09:27:27 +0100

    BasicTests

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Mazda.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Mazda.java
@@ -1,32 +1,32 @@
-package com.kodilla.collections.interfaces.homework;
-
-public class Mazda implements Car{
-
-    int speed;
-    private int acceleration;
-    private int brakes;
-
-    public Mazda(int speed, int acceleration, int brakes) {
-        this.speed = speed;
-        this.acceleration = acceleration;
-        this.brakes = brakes;
-    }
-    public int getSpeed() {
-
-        return speed;
-    }
-
-    public int getAcceleration() {
-        return acceleration;
-    }
-
-    public void increaseSpeed() {
-
-        speed*=acceleration;
-    }
-
-    public void decreaseSpeed() {
-
-        speed*=brakes;
-    }
-}
+//package com.kodilla.collections.interfaces.homework;
+//
+//public class Mazda implements Car{
+//
+//    int speed;
+//    private int acceleration;
+//    private int brakes;
+//
+//    public Mazda(int speed, int acceleration, int brakes) {
+//        this.speed = speed;
+//        this.acceleration = acceleration;
+//        this.brakes = brakes;
+//    }
+//    public int getSpeed() {
+//
+//        return speed;
+//    }
+//
+//    public int getAcceleration() {
+//        return acceleration;
+//    }
+//
+//    public void increaseSpeed() {
+//
+//        speed*=acceleration;
+//    }
+//
+//    public void decreaseSpeed() {
+//
+//        speed*=brakes;
+//    }
+//}
