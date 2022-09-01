2d97742feeb226fa45be5a18ccfe82c275dca22f
Krzysztof Antczak <antczakmd@gmail.com>
2022-09-01 10:17:46 +0100

    BasicTests

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Car.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/Car.java
@@ -1,11 +1,11 @@
-//package com.kodilla.collections.interfaces.homework;
-//
-//public class Car {
-//
-//    int getSpeed();
-//
-//    int getAcceleration();
-//    void increaseSpeed();
-//    void decreaseSpeed();
-//
-//}
+package com.kodilla.collections.interfaces.homework;
+
+public class Car {
+
+    int getSpeed();
+
+    int getAcceleration();
+    void increaseSpeed();
+    void decreaseSpeed();
+
+}
