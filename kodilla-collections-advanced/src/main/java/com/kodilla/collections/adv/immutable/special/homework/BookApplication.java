912750665e93ba9563e4ea700c01dcb0e07e8507
Krzysztof Antczak <antczakmd@gmail.com>
2022-09-06 13:46:45 +0100

    BasicTests

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -4,9 +4,19 @@ public class BookApplication {
     public static void main(String[] args) {
 
         BookManager.createBook("Really?", "Jeremy Clarkson");
-        BookManager.createBook("The Top Gear Years","Jeremy Clarkson");
-        BookManager.createBook("The Top Gear Years","Jeremy Clarkson");
+        Book bookOne = new Book("The Top Gear Years","Jeremy Clarkson");
+        BookManager.bookDirectory.add(bookOne);
+        Book bookTwo = new Book("The Top Gear Years","Jeremy Clarkson");
+        BookManager.bookDirectory.add(bookTwo);
 
+        System.out.println("All tokens: " + BookManager.bookDirectory);
+        System.out.println("Directory size: " + BookManager.bookDirectory.size());
 
+        System.out.println("Do they have same values?");
+        System.out.println(bookOne.equals(bookTwo));
+        if (bookOne.equals(bookTwo)) {
+            System.out.println("Is it the same book?");
+            System.out.println(bookOne == bookTwo);
+        }
     }
 }
