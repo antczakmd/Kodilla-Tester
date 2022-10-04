2309e6affcb639ab9ca776996dc96bb9d45d2c42
Krzysztof Antczak <antczakmd@gmail.com>
2022-10-04 12:29:05 +0100

    Zadanie 6.4

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -5,13 +5,13 @@ public class Warehouse {
 
     private static Set<Order> orderList = new HashSet<>();
 
-    public static Order addOrder(Order order) {
+    public Order addOrder(Order order) {
         orderList.add(order);
         System.out.println("Order " + order + " added.");
-        return order;
+
     }
 
-    public static Order getOrder(String number) throws OrderDoesntExistException {
+    public Order getOrder(String number) throws OrderDoesntExistException {
         System.out.println("Searching for order No. " + number);
         return orderList
                 .stream()
